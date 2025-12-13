02ebbe399663ce403b7ecdee6fcdfedf5df9b349
Leoo_Rocket <leo@leoodesign.com.br>
2025-12-13 07:53:55 -0300

    Pet Funny: backend PostgreSQL + admin completo

--- server.js
+++ server.js
@@ -374,7 +374,7 @@ app.delete('/api/bookings/:id', async (req, res) => {
 ========================= */
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'roleta.html'));
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
 app.get('/admin', (req, res) => {
